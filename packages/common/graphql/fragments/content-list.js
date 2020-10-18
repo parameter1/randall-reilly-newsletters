ce523a6905c781a9f2ce34851a71acfc28173541
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-18 14:05:10 -0500

    Add fields to content list fragment

--- packages/common/graphql/fragments/content-list.js
+++ packages/common/graphql/fragments/content-list.js
@@ -4,12 +4,49 @@ module.exports = gql`
 
 fragment NewsletterContentListFragment on Content {
   id
+  type
   name(input: { mutation: Email })
+  teaser(input: { mutation: Email, useFallback: false, maxLength: null })
+  labels
+  published
+  company {
+    id
+    type
+    name
+    siteContext {
+      path
+    }
+  }
+  primarySection {
+    id
+    name
+    alias
+    fullName
+    canonicalPath
+  }
   primaryImage {
     id
-    src
+    src(input: { options: { auto: "format,compress", q: 70 } })
     alt
+    isLogo
+  }
+  ... on ContentPromotion {
+    body(input: { mutation: Email })
+    linkText
+    linkUrl
+  }
+  ...on ContentNews {
+    byline
+  }
+  ...on Authorable {
+    authors {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
   }
 }
-
 `;
