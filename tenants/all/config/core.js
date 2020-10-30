b3a37c92990cbce1ba5e6b7121f10b26c21adf3e
Brandon Krigbaum <brandonbk@gmail.com>
2020-10-29 21:38:09 -0500

    Add ovd-daily config

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -4,6 +4,23 @@ const config = {
       enabled: true,
     },
   },
+  'ovd-daily': {
+    logoSrc: '/files/base/randallreilly/all/image/static/ovd-logo.png',
+    subscribeLink: 'https://www.overdrive.com/subscribe',
+    contactLinks: [
+      { href: 'https://www.google.com', label: 'Advertising' },
+      { href: 'https://www.google.com', label: 'Editorial' },
+    ],
+    socialMedia: {
+      imagePath: '/files/base/randallreilly/all/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/OverdriveTrucking/', target: '_blank' },
+        { provider: 'twitter', href: 'https://twitter.com/overdriveupdate', target: '_blank' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/showcase/overdrive-partner-solutions/', target: '_blank' },
+        { provider: 'youtube', href: 'https://www.youtube.com/channel/UCCJiBY50M2ie-QBjdDU4p1A', target: '_blank' },
+      ],
+    },
+  },
 };
 
 module.exports = config;
