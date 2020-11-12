877e329c16a2418c20661e9d8af49ce158a9a8f3
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-11 21:31:20 -0600

    Add ccj-daily

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -25,6 +25,24 @@ const config = {
       ],
     },
   },
+  'ccj-daily': {
+    name: 'Daily Report',
+    logoSrc: '/files/base/randallreilly/all/image/static/ccj-logo-white.png',
+    subscribeLink: 'https://www.ccjdigital.com/subscribe',
+    contactLinks: [
+      { href: 'https://www.google.com', label: 'Advertising' },
+      { href: 'https://www.google.com', label: 'Editorial' },
+    ],
+    socialMedia: {
+      imagePath: '/files/base/randallreilly/all/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/CCJMagazine/', target: '_blank' },
+        { provider: 'twitter', href: 'https://twitter.com/ccjdigital', target: '_blank' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/commercial-carrier-journal/', target: '_blank' },
+        { provider: 'youtube', href: 'https://www.youtube.com/user/CCJDigital', target: '_blank' },
+      ],
+    },
+  },
 };
 
 module.exports = config;
