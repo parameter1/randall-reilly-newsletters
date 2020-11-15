ac851c88a111b79b4e3b59e517e41446ac266bf7
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-14 20:29:31 -0600

    Add remaining weekly newsletters

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -16,12 +16,32 @@ const config = {
     name: 'Daily Report',
     ...brands.ccj,
   },
+  'ccj-equipment-weekly': {
+    name: 'Equipment Weekly',
+    ...brands.ccj,
+  },
+  'ccj-technology-weekly': {
+    name: 'Technology Weekly',
+    ...brands.ccj,
+  },
+  'eqw-better-roads': {
+    name: 'Better Roads',
+    ...brands.eqw,
+  },
+  'eqw-big-iron-dealer': {
+    name: 'Big Iron Dealer',
+    ...brands.eqw,
+  },
   'eqw-daily': {
     name: 'Daily Report',
     ...brands.eqw,
   },
   'ovd-breaking-news': {
     name: 'Breaking News Alert',
+    ...brands.ovd,
+  },
+  'ovd-custom-rigs': {
+    name: 'Custom Rigs',
     ...brands.ovd,
   },
   'ovd-daily': {
@@ -36,8 +56,20 @@ const config = {
     name: 'Daily Report',
     ...brands.tn,
   },
+  'tn-she-drives': {
+    name: 'She Drives',
+    ...brands.tn,
+  },
   'tps-daily': {
     name: 'Daily Report',
+    ...brands.tps,
+  },
+  'tps-successful-dealer': {
+    name: 'Successful Dealer',
+    ...brands.tps,
+  },
+  'tps-weekly': {
+    name: 'Weekly Report',
     ...brands.tps,
   },
 };
