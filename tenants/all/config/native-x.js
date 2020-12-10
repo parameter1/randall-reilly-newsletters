851ea6312a05354aba1bbe99b11f8a5865ca6542
Jacob Bare <jacob@parameter1.com>
2020-12-09 23:10:46 -0600

    Configure NX placements for OVD and use on daily layout

---
+++ tenants/all/config/native-x.js
@@ -0,0 +1,13 @@
+module.exports = {
+  uri: 'https://randallreilly.native-x.parameter1.com',
+  placements: {
+    'ovd-daily': {
+      sponsor: '5fd192ccc19f3b0001d71741',
+      'rotation-a': '5fd192d7b192ef00018433d0',
+      'rotation-b': '5fd192dac19f3b0001d7175d',
+      'rotation-c': '5fd192dfc19f3b0001d71767',
+      'rotation-d': '5fd192e2b192ef00018433e3',
+      'rotation-e': '5fd192e6c19f3b0001d7177a',
+    },
+  },
+};
