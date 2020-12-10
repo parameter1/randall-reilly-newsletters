8ca2f871ce5ac1ea7960262ca84f7cd4f9469723
Jacob Bare <jacob@parameter1.com>
2020-12-09 22:23:49 -0600

    Create date to timestamp

--- packages/common/native-x/index.js
+++ packages/common/native-x/index.js
@@ -1,7 +1,9 @@
+const dateToTimestamp = require('./date-to-timestamp');
 const fetchEmailAd = require('./fetch-email-ad');
 const isEnabled = require('./is-enabled');
 
 module.exports = {
+  dateToTimestamp,
   fetchEmailAd,
   isEnabled,
 };
