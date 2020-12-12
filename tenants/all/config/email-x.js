483c46d1169ba1a35ef5f529074f0344f2f25a86
Brandon Krigbaum <brandonbk@gmail.com>
2020-12-12 14:22:03 -0600

    Add emailX placement ids

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -3,55 +3,10 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://randallreilly.serve.email-x.parameter1.com');
 
 config
-  .setAdUnits('ovd-daily', [
+  .setAdUnits('ccj-breaking-news', [
     {
       name: 'sponsor',
-      id: '5f9c04eb32dc3ebbfaf3efc5',
-      width: 600,
-      height: 100,
-    },
-  ])
-
-  .setAdUnits('ovd-daily', [
-    {
-      name: 'rotation-a',
-      id: '5f874a0f32dc3e17dff3e9a0',
-      width: 600,
-      height: 100,
-    },
-  ])
-
-  .setAdUnits('ovd-daily', [
-    {
-      name: 'rotation-b',
-      id: '5f874a1f32dc3e761af3e9aa',
-      width: 600,
-      height: 100,
-    },
-  ])
-
-  .setAdUnits('ovd-daily', [
-    {
-      name: 'rotation-c',
-      id: '5f874b7a32dc3e2b0ef3e9c6',
-      width: 600,
-      height: 100,
-    },
-  ])
-
-  .setAdUnits('ovd-daily', [
-    {
-      name: 'rotation-d',
-      id: '5f874baa1949735baf5f5a74',
-      width: 600,
-      height: 100,
-    },
-  ])
-
-  .setAdUnits('ovd-daily', [
-    {
-      name: 'rotation-e',
-      id: '5f9c04c832dc3ec3e3f3efb2',
+      id: '5fd50754194973b7d35f798f',
       width: 600,
       height: 100,
     },
@@ -64,48 +19,535 @@ config
       width: 600,
       height: 100,
     },
-  ])
-
-  .setAdUnits('ccj-daily', [
     {
       name: 'rotation-a',
       id: '5fa813341949731cc25f714a',
       width: 600,
       height: 100,
     },
-  ])
-
-  .setAdUnits('ccj-daily', [
     {
       name: 'rotation-b',
       id: '5fa8135119497380e85f715d',
       width: 600,
       height: 100,
     },
-  ])
-
-  .setAdUnits('ccj-daily', [
     {
       name: 'rotation-c',
       id: '5fa8136832dc3ee2fbf3ff05',
       width: 600,
       height: 100,
     },
-  ])
-
-  .setAdUnits('ccj-daily', [
     {
       name: 'rotation-d',
       id: '5fa8137532dc3e2872f3ff18',
       width: 600,
       height: 100,
     },
-  ])
-
-  .setAdUnits('ccj-daily', [
     {
       name: 'rotation-e',
       id: '5fa813811949732de15f718b',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('ccj-technology-weekly', [
+    {
+      name: 'sponsor',
+      id: '5fc7b3db1949735d2d5f75a1',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fc7b40832dc3e83aef4029b',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fc7b4191949737eba5f75f3',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fc7b4281949733ceb5f7606',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fc7b43632dc3ec5daf402c0',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('ccj-equipment-weekly', [
+    {
+      name: 'sponsor',
+      id: '5fd50bc132dc3eb4b0f40715',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd50bee32dc3e4e28f40731',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd50c0032dc3e6e96f4074d',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd50c1532dc3ec2a2f40757',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd50c29194973816d5f7a3b',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('eqw-better-roads', [
+    {
+      name: 'sponsor',
+      id: '5fd50ce01949735e935f7ace',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd50d0532dc3e1e01f407fb',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd50d8f194973f95a5f7aea',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd50dae32dc3e1decf40829',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: 'TBD',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('eqw-big-iron-dealer', [
+    {
+      name: 'sponsor',
+      id: '5fd5152b19497312045f7b45',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd5153c32dc3e7da7f40890',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd5156219497317c05f7b6a',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd5157519497350885f7b74',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd5158832dc3eb31bf408b5',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('eqw-daily', [
+    {
+      name: 'sponsor',
+      id: '5fd515a519497300285f7ba2',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd515ba19497360835f7bbe',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd515e532dc3eee8ff408fe',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd5160232dc3eb78ef40911',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd5161a32dc3ee09ef40924',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-e',
+      id: '5fd5163232dc3ef8a3f4092e',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+
+  .setAdUnits('hwt-weekly', [
+    {
+      name: 'sponsor',
+      id: '5fd525b41949732ac65f8036',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd525c419497313805f8049',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd525d6194973d8f55f8053',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd525ef1949731d3b5f805d',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd5261732dc3ec916f40d65',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('ovd-breaking-news', [
+    {
+      name: 'sponsor',
+      id: '5fd5166932dc3ec26ef4096f',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('ovd-custom-rigs', [
+    {
+      name: 'sponsor',
+      id: '5fd5167e32dc3e4af9f40982',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd5169b1949736dd95f7c62',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd516b032dc3ef56ff40995',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd516c632dc3e3ae1f409a8',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd516e332dc3e900ef409b2',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('ovd-daily', [
+    {
+      name: 'sponsor',
+      id: '5f9c04eb32dc3ebbfaf3efc5',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5f874a0f32dc3e17dff3e9a0',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5f874a1f32dc3e761af3e9aa',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5f874b7a32dc3e2b0ef3e9c6',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5f874baa1949735baf5f5a74',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-e',
+      id: '5f9c04c832dc3ec3e3f3efb2',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tlc-daily', [
+    {
+      name: 'sponsor',
+      id: '5fd521a019497331c25f7d99',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd521ad32dc3e3e58f40abf',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd521ca1949733ca05f7dbe',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd521e232dc3ebc9ef40ae4',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd5221f19497357ab5f7dda',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-e',
+      id: '5fd522311949735e705f7de4',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tn-daily', [
+    {
+      name: 'sponsor',
+      id: '5fd5227832dc3e465df40b51',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd5228f194973e2885f7e36',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd5229e19497383ac5f7e49',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd522b71949738b4c5f7e5c',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd522d21949736f1b5f7e6f',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-e',
+      id: '5fd522e319497303c85f7e82',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tn-she-drives', [
+    {
+      name: 'sponsor',
+      id: '5fd522fb194973af135f7e9e',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd5230932dc3e7885f40bb5',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd52319194973f3bb5f7eba',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd5233632dc3e36caf40bd1',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd5234f32dc3eff7bf40be4',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tps-daily', [
+    {
+      name: 'sponsor',
+      id: '5fd523ca194973987d5f7f03',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd523e032dc3e4cadf40c24',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd523f232dc3eedecf40c2e',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd524051949730b955f7f28',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd52417194973573a5f7f3b',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-e',
+      id: '5fd5242e19497372645f7f4e',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tps-successful-dealer', [
+    {
+      name: 'sponsor',
+      id: '5fd52449194973fec45f7f6a',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd524871949731b255f7f7d',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd5249a32dc3e1e63f40c9b',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd524c7194973464b5f7fa2',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd524d732dc3e3204f40ca5',
+      width: 600,
+      height: 100,
+    },
+  ])
+
+  .setAdUnits('tps-weekly', [
+    {
+      name: 'sponsor',
+      id: '5fd524fd32dc3e4ceef40cca',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-a',
+      id: '5fd52510194973261c5f7fe2',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-b',
+      id: '5fd52523194973abdb5f7ff5',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-c',
+      id: '5fd5253532dc3e7d31f40cf8',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'rotation-d',
+      id: '5fd52597194973b1e05f8008',
       width: 600,
       height: 100,
     },
