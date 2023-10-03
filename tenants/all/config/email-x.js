8ad76a4449b0bd84b752253da54e2a1d0e03e259
Brandon Krigbaum <brandonbk@gmail.com>
2023-10-03 07:24:17 -0500

    Use compat api for email-x and native-x

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -1,6 +1,6 @@
 const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
 
-const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://randallreilly.serve.email-x.parameter1.com');
+const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://delivery.mindfulcms.com/rr/default/compat/email-banner');
 
 config
   .setAdUnits('ccj-breaking-news', [
